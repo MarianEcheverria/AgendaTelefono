29701e33d60f0c35af1fff41137fe8e5d6566ab8
Mariandre Echeverria <mariandreed@outlook.es>
2024-08-12 21:18:41 -0600

    siuuuuu

--- src/agendatelefonica/AgendaTelefonica.java
+++ src/agendatelefonica/AgendaTelefonica.java
@@ -7,39 +7,40 @@ public class AgendaTelefonica {
     public static void main(String[] args) {
         numero datos[] = new numero[15];
         Scanner entrada = new Scanner(System.in);
+        Scanner letra = new Scanner(System.in);
         int op = 0;
 
         while (op != 5) {
             System.out.println("----Menu----");
             System.out.println("1. guardar numero");
-            System.out.println("2. buscar");
-            System.out.println("3. encontrar");
+            System.out.println("2. buscar individual");
+            System.out.println("3. ver todo");
             System.out.println("4. modificar");
             System.out.println("5. salir");
             op = entrada.nextInt();
             if (op == 1) {
-                System.out.println("ingrese nombre ");
-                String nombre = entrada.nextLine();
-                System.out.println("ingrese numero telefonico ");
+                System.out.println("Ingrese numero:  ");
                 int numerotel = entrada.nextInt();
-                System.out.println("ingrese el correo electronico");
-                String correo = entrada.nextLine();
+                System.out.println("Ingrese nombre: ");
+                String nombre = letra.nextLine();
+                System.out.println("Ingrese el correo electronico: ");
+                String correo = letra.nextLine();
                 System.out.println(nuevo(numerotel, nombre, correo, datos));
 
-            } else if (op == 2) {
-                System.out.println(imprimirtodo(datos));
             } else if (op == 3) {
+                System.out.println(imprimir(datos));
+            } else if (op == 2) {
                 System.out.println("ingrese numero");
                 int numerotel = entrada.nextInt();
-                System.out.println(buscares(numerotel, datos));
+                System.out.println(buscar(numerotel, datos));
             } else if (op == 4) {
                 System.out.println("ingrese numero para modificar");
                 int numerotel = entrada.nextInt();
-                if (modificar(numerotel, datos)) {
+                if (modificardato(numerotel, datos)) {
                     System.out.println("ingrese el nombre");
-                    String nombre = entrada.nextLine();
+                    String nombre = letra.nextLine();
                     System.out.println("ingrese el correo");
-                    String correo = entrada.nextLine();
+                    String correo = letra.nextLine();
                 } else {
                     System.out.println("no existe el numero");
                 }
@@ -58,16 +59,56 @@ public class AgendaTelefonica {
                 break;
 
             } else if (arreglo[i].getNumerotel() == numerotel) {
-                return "ya existe el numero";
+                return " \n ¡NUMERO EXISTENTE!";
             }
 
         }
         if (!lleno) {
-            return "se guardo con exito";
+            return " \n ¡Se Guardo Con Exito! \n";
         } else {
-            return "espacio lleno";
+            return "¡¡ESPACIO LLENO!!";
         }
 
     }
-public static String modificar
+
+    public static String imprimir(numero arreglo[]) {
+        String cadena = "";
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] != null) {
+                cadena = cadena + arreglo[i].getNumerotel() + "|" + arreglo[i].getNombre() + "|" + arreglo[i].getCorreo() + "\n";
+            }
+        }
+        return cadena;
+    }
+
+    public static boolean modificardato(int numerotel, numero arreglo[]) {
+
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] != null && arreglo[i].getNumerotel() == numerotel) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static String modificar(int numerotel, String nombre, String correo, numero arreglo[]) {
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] != null && arreglo[i].getNumerotel() == numerotel) {
+                arreglo[i].setNombre(nombre);
+                arreglo[i].setCorreo(correo);
+                return "¡Se Actualizo Con Exito!";
+            }
+
+        }
+        return "no se modifico";
+    }
+
+    public static String buscar(int numerotel, numero arreglo[]) {
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] != null && arreglo[i].getNumerotel() == numerotel) {
+                return "los datos \n nombre: " + arreglo[i].getNombre() + " " + "correo : " + arreglo[i].getCorreo();
+            }
+        }
+        return " \n NO EXISTE \n";
+    }
 }
